3b7eb59ba03dceb7a39e92586e68d670f31fef09
alanrichter53@gmail.com <alanrichter53@gmail.com>
2025-03-24 20:00:39 +0100

    Added high score functionality.

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
+    int highScore;
     public Text scoreText;
+    public Text highScoreText;
     public GameObject gameOverScreen;
     public AudioSource scoreSound;
 
+    private void Update()
+    {
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("highScore", playerScore);
+    }
     [ContextMenu("Increase Score")]
     public void AddScore(int scoreToAdd)
     {
@@ -24,6 +31,13 @@ public class LogicScript : MonoBehaviour
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
+        if (playerScore > highScore)
+        {
+            highScore = playerScore;
+            PlayerPrefs.SetInt("highScore", playerScore);
+            highScoreText.text = "High Score: " + highScore.ToString();
+            PlayerPrefs.Save();
+        }
     }
 
     public void PlayGame()
